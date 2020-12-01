1a5d05a7a4f15cfe781b7ca079f2eed51153df9d
you-cheng <andychen208@gmail.com>
2020-12-01 14:47:02 +0800

    sql_module to prototype
    change the function in sqlite_module.js to prototype.
    
    make some change prepare to use ejs engine.
    
    consider to use express-generator.

--- server/sqlite_module.js
+++ server/sqlite_module.js
@@ -2,59 +2,58 @@ const e = require('express');
 
 function sql(db_name = 'hotDong.db') {
     this.db_name = db_name;
+}
 
-    this.init_db = function () {
-        var sqlite3 = require('sqlite3').verbose();
-        var db = new sqlite3.Database(__dirname + '/' + this.db_name, function (e) {
+sql.prototype.init_db = function () {
+    var sqlite3 = require('sqlite3').verbose();
+    var db = new sqlite3.Database(__dirname + '/' + this.db_name, function (e) {
+        if (e) throw e;
+    });
+    db.serialize(function () {
+        db.run("CREATE TABLE IF NOT EXISTS simple_transaction(\
+        transaction_ID INTEGER, \
+        transaction_date NUMERIC NOT NULL, \
+        sheet_num INTEGER NOT NULL, \
+        normal_count INTEGER NOT NULL DEFAULT 0, \
+        fort_count INTEGER NOT NULL DEFAULT 0, \
+        french_count INTEGER NOT NULL DEFAULT 0, \
+        payment_status NUMERIC NOT NULL DEFAULT 0, \
+        PRIMARY KEY(transaction_ID AUTOINCREMENT) \
+        ) ");
+
+    })
+    db.close();
+}
+
+sql.prototype.insert_order = function (data) {
+    var sqlite3 = require('sqlite3').verbose();
+    var db = new sqlite3.Database(__dirname + '/' + this.db_name, function (e) {
+        if (e) throw e;
+    });
+
+    db.serialize(function () {
+        var stmt = db.prepare("INSERT INTO simple_transaction (transaction_date, sheet_num, normal_count, fort_count, french_count) VALUES (DateTime('now'), $sheet, $normal, $fort, $french)");
+        stmt.run(data);
+        stmt.finalize();
+    });
+
+    db.close();
+}
+
+sql.prototype.confirm_order = function (data) {
+    var sqlite3 = require('sqlite3').verbose();
+    var db = new sqlite3.Database(__dirname + '/' + this.db_name, function (e) {
+        if (e) throw e;
+    });
+    db.serialize(function () {
+        db.get('SELECT transaction_ID FROM simple_transaction WHERE sheet_num = $checked_sheet_num ORDER BY transaction_ID DESC LIMIT 1', data, function (e, row) {
             if (e) throw e;
-        });
-        db.serialize(function () {
-            db.run("CREATE TABLE IF NOT EXISTS simple_transaction(\
-            transaction_ID INTEGER, \
-            transaction_date NUMERIC NOT NULL, \
-            sheet_num INTEGER NOT NULL, \
-            normal_count INTEGER NOT NULL DEFAULT 0, \
-            fort_count INTEGER NOT NULL DEFAULT 0, \
-            french_count INTEGER NOT NULL DEFAULT 0, \
-            payment_status NUMERIC NOT NULL DEFAULT 0, \
-            PRIMARY KEY(transaction_ID AUTOINCREMENT) \
-            ) ");
+            if (!(row === undefined)) {
+                db.run(`UPDATE simple_transaction SET payment_status = 1 WHERE transaction_ID = ${row.transaction_ID}`, function (e) { if (e) throw e; })
+            }
 
         })
-        db.close();
-    }
-
-    this.insert_order = function (data) {
-        var sqlite3 = require('sqlite3').verbose();
-        var db = new sqlite3.Database(__dirname + '/' + this.db_name, function (e) {
-            if (e) throw e;
-        });
-
-        db.serialize(function () {
-            var stmt = db.prepare("INSERT INTO simple_transaction (transaction_date, sheet_num, normal_count, fort_count, french_count) VALUES (DateTime('now'), $sheet, $normal, $fort, $french)");
-            stmt.run(data);
-            stmt.finalize();
-        });
-
-        db.close();
-    }
-
-    this.confirm_order = function (data) {
-        var sqlite3 = require('sqlite3').verbose();
-        var db = new sqlite3.Database(__dirname + '/' + this.db_name, function (e) {
-            if (e) throw e;
-        });
-        db.serialize(function () {
-            db.get('SELECT transaction_ID FROM simple_transaction WHERE sheet_num = $checked_sheet_num ORDER BY transaction_ID DESC LIMIT 1', data, function (e, row) {
-                if (e) throw e;
-                if (!(row === undefined)) {
-                    db.run(`UPDATE simple_transaction SET payment_status = 1 WHERE transaction_ID = ${row.transaction_ID}`, function (e) { if (e) throw e; })
-                }
-
-            })
-        })
-
-    }
+    })
 
 }
 
---
+++ views/index_beta.js
@@ -0,0 +1,192 @@
+var storage = localStorage;
+var now_sheet_number = 1
+var now_total_price = 0
+var show_order_sheet
+var show_order_price
+
+function ShowTime() {
+    var NowDate = new Date();
+    var year = NowDate.getFullYear();
+    var month = NowDate.getMonth();
+    var day = NowDate.getDate();
+    var hour = NowDate.getHours();
+    var minute = NowDate.getMinutes();
+    var sec = NowDate.getSeconds();
+    document.getElementById('datebox').innerHTML = year + '年' + month + '月' + day + '日';
+    document.getElementById('timebox').innerHTML = hour + '時' + minute + '分' + sec + '秒';
+    setTimeout('ShowTime()', 1000);
+}
+
+function get_total_price() {
+    var purchase_items = $(':checked')
+    var price_dict = {
+        hot_dog_fort_price: 10,
+        normal_hot_dog_price: 20,
+        french_fries_dog_price: 30,
+    }
+
+    now_total_price = 0
+    for (var item of purchase_items) {
+        now_total_price += item.value * price_dict[item.name + '_price']
+    }
+    $("#price_show").text(now_total_price)
+}
+
+function update_serial_number() {
+    now_sheet_number = now_sheet_number + 1;
+    $('#sheet_number').val(now_sheet_number);
+}
+
+function add_check_table() {
+    var template = `<table id="sheet_${now_sheet_number}" class="check_table">
+            <tr>
+                <td>單號</td>
+                <td>${now_sheet_number}</td>
+            </tr>
+            <tr>
+                <td>金額</td>
+                <td>${now_total_price}</td>
+            </tr>
+        </table>`
+    $("#check_area_container").append(template);
+
+}
+
+function get_order_detail() {
+    var show = $(this).find('td:odd');
+    show_order_sheet = show[0].innerHTML;
+    show_order_price = show[1].innerHTML;
+}
+
+function show_order_detail() {
+    var order_detail = JSON.parse(storage['sheet' + show_order_sheet]);
+    var template = `<table id="show_${show_order_sheet}" class="detail_table">
+                <tr>
+                    <td>單號:${show_order_sheet}</td>
+                </tr>
+                <tr>
+                    <td>果粉熱狗:${order_detail.normal_hot_dog}</td>
+                </tr>
+                <tr>
+                    <td>熱狗堡:${order_detail.hot_dog_fort}</td>
+                </tr>
+                <tr>
+                    <td>薯條熱狗:${order_detail.french_fries_dog}</td>
+                </tr>
+                <tr>
+                    <td>金額:${show_order_price}</td>
+                </tr>
+            </table>
+            <button id="confirm_button" class="button">確認結帳</button>
+            <button class="button">取消訂單</button>`
+    $('#detail_table_area').empty()
+        .prepend(template);
+}
+
+function reset_form() {
+    $("#first_hot_dog_fort").prop("checked", true);
+    $("#first_normal_hot_dog").prop("checked", true);
+    $("#first_french_fries_dog").prop("checked", true);
+    $("#price_show").text(0);
+    now_total_price = 0;
+}
+
+function set_sheet_storage() {
+    var purchase_items = $(':checked')
+    var data = {
+        sheet_number: now_sheet_number,
+        total_price: now_total_price
+    }
+    for (var item of purchase_items) {
+        data[item.name] = item.value
+    }
+    storage['sheet' + now_sheet_number] = JSON.stringify(data)
+}
+
+function finish_payment() {
+    $('#detail_area_container').on('click', '#confirm_button', function () {
+        $.ajax({
+            url: '/payment',                        // url位置
+            type: 'post',                   // post/get
+            data: { checked_sheet_number: show_order_sheet }      // 輸入的資料
+        });
+    })
+}
+
+function clean_order() {
+    $('#detail_area_container').on('click', '.button', function () {
+        $('#detail_table_area').empty();
+        $('#sheet_' + show_order_sheet).remove();
+        delete storage['sheet' + show_order_sheet];
+    })
+}
+
+function restore_unfinish_order() {
+    $('#check_area_container>table').remove();
+    var sorted_keys = Object.keys(storage).sort(function (a, b) {
+        return a.localeCompare(b, undefined, { numeric: true });
+    })
+    var max_sheet_number = 0
+    for (var i = 0; i < sorted_keys.length; i++) {
+        var key = sorted_keys[i]
+        if (!(key.startsWith('sheet'))) { continue; }
+        var value = JSON.parse(storage[key]);
+        let restore_sheet_number = value.sheet_number;
+        var restore_total_price = value.total_price;
+        if (restore_sheet_number > max_sheet_number) max_sheet_number = restore_sheet_number;
+        var template = `<table id="sheet_${restore_sheet_number}" class="check_table">
+            <tr>
+                <td>單號</td>
+                <td>${restore_sheet_number}</td>
+            </tr>
+            <tr>
+                <td>金額</td>
+                <td>${restore_total_price}</td>
+            </tr>
+        </table>`
+        $("#check_area_container").append(template);
+    }
+    now_sheet_number = max_sheet_number + 1
+    $('#sheet_number').val(now_sheet_number);
+
+
+
+}
+
+function clean_storage() {
+    for (const key in storage) {
+        if (storage.hasOwnProperty(key) && (key.startsWith('sheet'))) {
+            storage.removeItem(key);
+        }
+    }
+}
+
+function mount_ajaxForm() {
+    $('#order_form').ajaxForm(function () {
+        add_check_table()
+        set_sheet_storage()
+        update_serial_number()
+        reset_form()
+    });
+}
+
+$(function () {
+    $('#check_area_container').css('height', window.innerHeight * 0.9)
+    $('table input').on('click', get_total_price)
+    $('#check_area_container').on('click', '.check_table', function () {
+        $(this).addClass('checked_table');
+        $(this).siblings('.check_table').removeClass('checked_table')
+    })
+    // 讓detail_area帶有check_area的狀態
+    $('#check_area_container').on('click', '.check_table', function () {
+        get_order_detail.call(this);
+        show_order_detail();
+    })
+    $('#restore_button').on('click', restore_unfinish_order)
+    $('#clean_storage_button').on('click', clean_storage)
+
+    ShowTime()
+    mount_ajaxForm()
+    finish_payment()
+    clean_order()
+})
