6c4075b6790b21291e9f06355e5bf857b43903ff
you-cheng <andychen208@gmail.com>
2020-12-10 08:02:35 +0800

    init

---
+++ README.md
@@ -0,0 +1,23 @@
+# pos-nodejs
+使用說明：
+1. 先安裝Node.js 8.11.4(為當初開發環境，可自行安裝新版測試)
+2. 在目標資料夾中下指令 npm install
+3. 目標資料夾中開啟cmd執行下指令 node app.js
+
+希望利用nodejs寫出一個簡單的pos系統，可以實際在夜市佈署
+目前大致架構
+1. 前端頁面利用ajax向後端傳值
+2. nodejs充當router並且對db操作
+3. 選擇sqlite當作db
+4. 利用local storage製作還原鍵
+
+未實踐部分
+1. 在平板上運行時，似乎需要修改點擊物件的大小
+2. 資料庫做成關聯式
+
+關聯式資料庫雛形
+- table：orders、products、orders_products
+1. order：order_ID(pk), order_datetime, total_price, payment_status
+2. product: product_ID(pk), prdocut_name, produc_price
+3. orders: order_id(pf), product_id(pf), quantity, tomato_sauce, mustard_sauce, both_sauce
+
---
+++ app.js
@@ -0,0 +1,64 @@
+var express = require("express");
+var path = require('path');
+var bodyParser = require("body-parser");
+var sqilte_module = require('./models/sqlite_module.js');
+var sql = new sqilte_module.sql('hot_dog.db');
+var app = express();
+sql.init_db()
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, '/views')))
+app.set('view engine', 'ejs');
+index_ejs = {
+    product1: {
+        html_name: '粿粉熱狗',
+        input_name: "normal_hot_dog"
+    },
+    product2: {
+        html_name: '熱狗堡',
+        input_name: "hot_dog_fort"
+    },
+    product3: {
+        html_name: '薯條熱狗',
+        input_name: "french_fries_dog"
+    }
+}
+app.get('/', function (req, res) {
+    res.render('index', index_ejs)
+});
+
+
+app.post('/send_order', function (req, res) {
+    var sheet_number = req.body.sheet_number;
+    var normal_number = req.body.normal_hot_dog;
+    var fort_number = req.body.hot_dog_fort;
+    var french_number = req.body.french_fries_dog;
+    var data = {
+        $sheet: sheet_number,
+        $fort: fort_number,
+        $normal: normal_number,
+        $french: french_number
+    }
+    sql.insert_order(data);
+
+    // 希望傳值處理完後仍保持原本頁面
+    res.render('index', index_ejs);
+});
+
+
+app.post('/payment', function (req, res) {
+    var checked_sheet_number = req.body.checked_sheet_number;
+    var data = {
+        $checked_sheet_num: checked_sheet_number
+    };
+    sql.confirm_order(data);
+})
+
+app.get('*', function (req, res) {
+    res.status(404);
+    res.send('找不到網頁！');
+});
+
+app.listen(5438, function (req, res) {
+    console.log("網站伺服器在5438埠口開工了！");
+});
