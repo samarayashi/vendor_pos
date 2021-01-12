95e0d5eec5a68d040110a02302704b4ae222e618
you-cheng <andychen208@gmail.com>
2021-01-12 22:59:35 +0800

    sqlite table sync mysql table
    modify sqlite module init_db, the content  of "create table"

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ var express = require("express");
 var app = express();
 var bodyParser = require("body-parser");
 var products_ejs = require('./models/productsBean.js').productsBean;
-// var sqilte_module = require('./models/sqlite_module.js');
-var mysql = require('./models/mysql_module/use_mysql.js')
-var vendorSQL = new mysql.vendorSQL()
-// var sql = new sqilte_module.sql('hot_dog.db');
-// sql.init_db()
+// var mysql = require('./models/mysql_module/use_mysql.js')
+// var vendorSQL = new mysql.vendorSQL()
+var sqilte_module = require('./models/sqlite_module.js');
+var sql = new sqilte_module.sql('hot_dog.db');
+sql.init_db()
 
 const PORT = process.env.PORT || 5438
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,7 +25,7 @@ app.post('/send_order', function (req, res) {
     var product3_number = req.body[products_ejs.product3.input_name];
     var totoal_price = req.body.total_price;
     var data = [sheet_number, product1_number, product2_number, product3_number, 0, totoal_price, 0]
-    vendorSQL.insert_order(data);
+    sql.insert_order(data);
     // 希望傳值處理完後仍保持原本頁面
     res.render('index', products_ejs);
 });
--- models/sqlite_module.js
+++ models/sqlite_module.js
@@ -11,15 +11,16 @@ sql.prototype.init_db = function () {
     });
     db.serialize(function () {
         db.run("CREATE TABLE IF NOT EXISTS simple_transaction(\
-        transaction_ID INTEGER, \
-        transaction_date NUMERIC NOT NULL, \
-        sheet_num INTEGER NOT NULL, \
-        normal_count INTEGER NOT NULL DEFAULT 0, \
-        fort_count INTEGER NOT NULL DEFAULT 0, \
-        french_count INTEGER NOT NULL DEFAULT 0, \
-        payment_status NUMERIC NOT NULL DEFAULT 0, \
-        PRIMARY KEY(transaction_ID AUTOINCREMENT) \
-        ) ");
+            `transaction_ID` INTEGER NOT NULL,\
+            `transaction_date` datetime NOT NULL,\
+            `sheet_num` INTEGER NOT NULL, \
+            `normal_hot_dog` INTEGER NOT NULL DEFAULT '0', \
+            `corn_hot_dog` INTEGER NOT NULL DEFAULT '0', \
+            `french_fries_dog` INTEGER NOT NULL DEFAULT '0', \
+            `payment_status` NUMERIC NOT NULL DEFAULT '0', \
+            `total_price` INTEGER NOT NULL DEFAULT '0', \
+            `cancel` NUMERIC NOT NULL DEFAULT '0', \
+            PRIMARY KEY(transaction_ID AUTOINCREMENT) )");
 
     })
     db.close();
@@ -32,7 +33,7 @@ sql.prototype.insert_order = function (data) {
     });
 
     db.serialize(function () {
-        var stmt = db.prepare("INSERT INTO simple_transaction (transaction_date, sheet_num, normal_count, fort_count, french_count) VALUES (DateTime('now'), $sheet, $normal, $fort, $french)");
+        var stmt = db.prepare("INSERT INTO simple_transaction (transaction_date,sheet_num, normal_hot_dog, corn_hot_dog, french_fries_dog,  payment_status, total_price, cancel) VALUES (DateTime('now','+8 hour'), $sheet_num, $normal_hot_dog, $corn_hot_dog, $french_fries_dog,  $payment_status, $total_price, $cancel)");
         stmt.run(data);
         stmt.finalize();
     });
@@ -40,7 +41,7 @@ sql.prototype.insert_order = function (data) {
     db.close();
 }
 
-sql.prototype.confirm_order = function (data) {
+sql.prototype.update_payment = function (data) {
     var sqlite3 = require('sqlite3').verbose();
     var db = new sqlite3.Database(__dirname + '/' + this.db_name, function (e) {
         if (e) throw e;
@@ -56,6 +57,23 @@ sql.prototype.confirm_order = function (data) {
     })
 
 }
+
+// sql.prototype.get_unpayment = function (data) {
+//     var sqlite3 = require('sqlite3').verbose();
+//     var db = new sqlite3.Database(__dirname + '/' + this.db_name, function (e) {
+//         if (e) throw e;
+//     });
+//     db.serialize(function () {
+//         db.get('SELECT transaction_ID FROM simple_transaction WHERE sheet_num = $checked_sheet_num ORDER BY transaction_ID DESC LIMIT 1', data, function (e, row) {
+//             if (e) throw e;
+//             if (!(row === undefined)) {
+//                 db.run(`UPDATE simple_transaction SET payment_status = 1 WHERE transaction_ID = ${row.transaction_ID}`, function (e) { if (e) throw e; })
+//             }
+
+//         })
+//     })
+
+// }
 
 
 module.exports.sql = sql;
